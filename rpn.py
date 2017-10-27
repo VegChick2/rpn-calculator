67142b68950c4881fc5999a3bb603b7980161415
qiaotianyu <qiaotian@umich.edu>
2017-10-27 11:55:19 -0400

    init commit

---
+++ rpn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def calculate(myarg):
+    pass
+
+
+
+def main():
+    while True:
+        calculate(input("rpm calc> "))
+
+
+
+if __name__=='__main__':
+    main()
+
+
