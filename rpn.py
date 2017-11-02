b3d13be60d092fed39f5a34471d9e87e49cdfbc1
qiaotian <qiaotian@umich.edu>
2017-11-02 15:32:44 -0400

    final  version

--- rpn.py
+++ rpn.py
@@ -1,37 +1,35 @@
-#!/usr/bin/env python3
+import operator
 
-def add(a,b):
-    return a+b
 
-def subtract(a,b):
-    return b-a;
-
-ops = {
-      '+':add,
-      '-':subtract,
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
         try:
-            stack.append(int(token))
+            token = int(token)
+            stack.append(token)
         except ValueError:
-            arg1 = stack.pop()
+            function = operators[token]
             arg2 = stack.pop()
-            function = ops[token]
-            result = function(arg1,arg2)
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
             stack.append(result)
-    print(stack)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpm calc> "))
-
-
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
-
 
